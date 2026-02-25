ecfba292dc1ab4584eef0edd2b868ad4021bcd7f
valisoa01 <valisoatolotriniaina@gmail.com>
2026-02-25 21:58:43 +0300

    add Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,14 @@ import Header from './components/Header'
 
 function App() {
   return (
-    <div className="min-h-screen bg-white dark:bg-zinc-950 text-zinc-900 dark:text-zinc-100">
+<div
+      className="
+        min-h-screen
+        bg-white dark:bg-zinc-950
+        text-zinc-900 dark:text-zinc-100
+        transition-colors duration-300 ease-in-out
+      "
+    >
       <Header />
       <main className="max-w-6xl mx-auto px-4 py-12">
         <h1 className="text-4xl font-bold text-primary">Bienvenue sur mon Portfolio</h1>
